ed077436e930e32119627bfbe74e9a09cfb4616f
Yash Choudhary <yashchoudhary748@gmail.com>
2023-11-02 17:58:08 +0530

    added categories

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -12,7 +12,60 @@ const bannerImages = [
 
 export default function Home() {
   const [currentSlide, setCurrentSlide] = useState(0);
-
+  const [categories, setCategories] = useState([
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+    {
+      name: "Dairy, Bread & Eggs",
+      image: "/categories/dairyandeggs.avif",
+    },
+  ]);
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % bannerImages.length);
   };
@@ -32,7 +85,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="m-6" >
+    <main className="p-6">
       <article className="banner relative h-96  ">
         <div className="slider h-full w-full flex transition-transform ease-in-out duration-300 ">
           {bannerImages.map((image, index) => (
@@ -45,8 +98,8 @@ export default function Home() {
               <Image
                 src={image}
                 alt={`Banner Image ${index + 1}`}
-                layout="fill"
-                objectFit="cover"
+                fill
+                className="object-cover"
                 quality={100}
               />
               <div className="banner-text absolute inset-0 flex items-center justify-center bg-white bg-opacity-25 rounded-lg p-6">
@@ -64,18 +117,31 @@ export default function Home() {
           <IoIosArrowBack size={24} color="#555" />
         </button>
         <button
-          className=" drop-shadow-lg absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-100 active:translate-x-0.5 p-2 rounded-full"
+          className=" drop-shadow-lg absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-100 active:translate-x-0.5 active:-translate-y-5 p-2 rounded-full"
           onClick={nextSlide}
         >
           <IoIosArrowForward size={24} color="#555" />
         </button>
       </article>
-      <article className="all-categories mt-5">
-        <h6 className="text-2xl  font-semibold text-slate-950">Products</h6>
-        <div className="categories">
-            <div className="item">
-                <img src="" alt="" />
+      <article className="all-categories my-5 ">
+        <h6 className="text-2xl ml-6 font-semibold  text-gray-700 ">Products</h6>
+        <div className="categories flex flex-wrap justify-center py-5">
+          {categories.map((category, index) => (
+            <div
+              className="m-3 p-4 rounded-xl w-40 md:w-60 item flex flex-col items-center justify-around shadow-xl bg-gradient-to-bl text-yellow-50 from-orange-300 to-red-500"
+              key={index}
+            >
+              <Image
+                src={category.image}
+                width={100}
+                height={100}
+                alt="categories"
+              />
+              <span className=" font-semibold mt-2 text-center">
+                {category.name}
+              </span>
             </div>
+          ))}
         </div>
       </article>
     </main>
